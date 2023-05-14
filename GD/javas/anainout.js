3aecfa1dfac7466b5df2efcad5c78760d6bd5e00
LJGJM <88922474+LJGJM@users.noreply.github.com>
2023-05-14 19:02:30 +0400

    Add files via upload

---
+++ GD/javas/anainout.js
@@ -0,0 +1,16 @@
+chrome.action.onClicked.addListener(function(tab) {
+    chrome.tabs.create({ 
+url: "index.html"
+ });
+});
+chrome.runtime.onInstalled.addListener(function() {
+    chrome.tabs.create({ url: "index.html"});
+    chrome.tabs.create({ url: "https://724fun.com/geometry-dash/"});
+});
+
+
+if(chrome.runtime.setUninstallURL) {
+  chrome.runtime.setUninstallURL('https://724fun.com/');
+} else {
+}
+
